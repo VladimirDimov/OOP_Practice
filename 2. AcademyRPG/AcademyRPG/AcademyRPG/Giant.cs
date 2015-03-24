0a90a2400e17ac72ee266a35ec60d50be9c30518
VladimirDimov <vdimov1124@gmail.com>
2015-03-24 15:10:33 +0200

    Ecosystem

---
+++ 2. AcademyRPG/AcademyRPG/AcademyRPG/Giant.cs
@@ -0,0 +1,50 @@
+﻿namespace AcademyRPG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Giant : Character, IFighter, IGatherer
+    {
+        private bool isEnhanced = false;
+        private int attackPoints = 150;
+
+        public Giant(string name, Point position)
+            : base(name, position, 0)
+        {
+            this.HitPoints = 200;
+        }
+
+        public int AttackPoints
+        {
+            get { return this.attackPoints; }
+        }
+
+        public int DefensePoints
+        {
+            get { return 80; }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            WorldObject firstAvailable = availableTargets.FirstOrDefault(x => (x.Owner != 0 && x.Owner != this.Owner));
+            return availableTargets.IndexOf(firstAvailable);
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+                if (!isEnhanced)
+                {
+                    this.attackPoints += 100;
+                }
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ 2. AcademyRPG/AcademyRPG/AcademyRPG/Ninja.cs
@@ -0,0 +1,55 @@
+﻿namespace AcademyRPG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Ninja : Character, IFighter, IGatherer
+    {
+        private int attackPoints;
+
+        public Ninja(string name, Point position, int owner)
+            :base(name, position, owner)
+        {
+            this.attackPoints = 0;
+            this.HitPoints = 1;
+        }
+
+        public int AttackPoints
+        {
+            get { return this.attackPoints; }
+        }
+
+        public int DefensePoints
+        {
+            get { return int.MaxValue; }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            WorldObject target = availableTargets
+                .OrderByDescending(x => x.HitPoints)
+                .FirstOrDefault(x => (x.Owner != 0 && x.Owner != this.Owner));
+            return availableTargets.IndexOf(target);
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+                this.attackPoints += resource.Quantity * 2;
+                return true;
+            }
+            else if (resource.Type == ResourceType.Lumber)
+            {
+                this.attackPoints += resource.Quantity;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
